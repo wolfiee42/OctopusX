06cd009091109ab31cb39ab36349cd7971f5156c
mamun <morshed.intern1@gmail.com>
2024-08-11 10:50:32 +0600

    home page added

---
+++ src/Pages/ErrrorPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ErrrorPage = () => {
+  return (
+    <div>
+      error page
+    </div>
+  )
+}
+
+export default ErrrorPage
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,11 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import Main from "../Layout/Main"
+import Home from "../Pages/Home/Home";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Main/>,
+        errorElement: <div>Not Found</div>,
+        children: [
+            {
+                path: "/",
+                element: <Home/>,
+            },
+        ]
       },
 ])
 
