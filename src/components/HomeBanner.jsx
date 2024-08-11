3ffb7f99dcf3165d752ffbe6c23c86b7d07bd8d8
sajib-intern <sajib.intern@gmail.com>
2024-08-11 16:02:45 +0600

    h, heading frot size, find out button edited

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -9,19 +9,19 @@ export default function HomeBanner() {
   }, []);
 
   return (
-    <section className="h-screen bg-[rgb(255,0,0)] text-white text-center flex flex-col gap-10 justify-center items-center">
+    <section className="h-[calc(100vh-112px)] overflow-hidden w-full px-5 bg-[rgb(255,0,0)] text-white text-center flex flex-col gap-5 md:gap-10 justify-center items-center">
       <h1
-        className="max-w-4xl mx-auto text-6xl font-bold mb-4"
+        className="max-w-4xl mx-auto text-2xl md:text-6xl font-bold mb-4"
         data-aos="fade-up"
         data-aos-offset="100"
       >
         Innovating at the Heart of The Creator Economy
       </h1>
       <p
-        className="max-w-3xl mx-auto text-[18px]"
+        className="max-w-3xl mx-auto text-sm md:text-[18px] leading-relaxed"
         data-aos="fade-up"
         data-aos-offset="100"
-        data-aos-delay="200"
+        data-aos-delay="100"
       >
         <span className="font-bold">OctopusX</span> is a MediaTech startup that
         is building <span className="font-bold">The Creator Economy.</span>
@@ -31,10 +31,10 @@ export default function HomeBanner() {
       </p>
       <Link to="/projects" target="_blank">
         <button
-          className="bg-white text-black px-4 py-2 rounded mt-4"
+          className="bg-white text-black hover:text-[rgb(255,0,0)] px-2 md:px-4 py-2 rounded md:mt-4"
           data-aos="fade-up"
           data-aos-offset="100"
-          data-aos-delay="300"
+          data-aos-delay="200"
         >
           Find Out More
         </button>
