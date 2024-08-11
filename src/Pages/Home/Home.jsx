287d210fbacc2d58777bc190383a3ad93efd7f14
AmdadulRimon <rimon.intern@gmail.com>
2024-08-11 15:24:37 +0600

    add:adding react helmet with website title and icon

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import WhoWeAre from '../../components/WhoWeAre'
 import OurFlagshipProject from '../../components/OurFlagshipProject'
 import OurPlans from '../../components/OurPlans'
+import { Helmet } from 'react-helmet-async'
 
 const Home = () => {
   return (
     <>
+    <Helmet>
+      <title>OctopusX</title>
+      <link rel="icon" type="image/png" href="/octopus.png" />
+    </Helmet>
     {/* Components Done by Amdadul */}
      <WhoWeAre/> 
      <OurFlagshipProject/>
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './Routes/Router.jsx';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <HelmetProvider>
     <RouterProvider router={router} />
+    </HelmetProvider>
   </React.StrictMode>
 )
