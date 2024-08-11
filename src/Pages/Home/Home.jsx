2186ef4a58ab3a9c1e8c9484322cae2106f692dd
AmdadulRimon <rimon.intern@gmail.com>
2024-08-11 12:14:43 +0600

    add: Our Flag Ship Project section & Who We are Section

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import WhoWeAre from '../../components/WhoWeAre'
+import OurFlagshipProject from '../../components/OurFlagshipProject'
+
+const Home = () => {
+  return (
+    <>
+    {/* Components Done by Amdadul */}
+     <WhoWeAre/> 
+     <OurFlagshipProject/>
+     
+      {/* Amdadul Components End here */}
+    </>
+  )
+}
+
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif'],
+    },
+    colors:{
+      'primary': '#FF0000',
+    }
+    }
   },
   plugins: [],
 }
