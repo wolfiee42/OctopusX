7c3ef7d0ecb73a15defeaf97a7b1f371baf737d7
AmdadulRimon <rimon.intern@gmail.com>
2024-08-12 14:02:06 +0600

    add:adding form vadilading wiht shadcn

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import WhoWeAre from '../../components/WhoWeAre'
 import OurFlagshipProject from '../../components/OurFlagshipProject'
 import OurPlans from '../../components/OurPlans'
+import { Helmet } from 'react-helmet-async'
+import Contact from '../contract/Contact'
 
 const Home = () => {
   return (
     <>
     {/* Components Done by Amdadul */}
+      <Helmet>
+        <title>OctopusX</title>
+        <link rel="icon" type='image/jpeg' href="/icon.jpeg" />
+      </Helmet>
      <WhoWeAre/> 
      <OurFlagshipProject/>
      <OurPlans/>
+     <Contact/>
       {/* Amdadul Components End here */}
     </>
   )
