28af4aaad4e8ec6b37073edcd0faf2df9824f827
gazinahian <gazinahian.intern@gmail.com>
2024-08-14 16:22:44 +0600

    Creating Projects-About-Us

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,5 +1,8 @@
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div className="font-bold text-white bg-slate-950 py-40 px-5 ">
+      <h1 className="text-3xl mb-10 line-clamp-4">About Us</h1>
+      <p className="text-md lg:w-[50%] xl:w-[50%]">OctopusX is a MediaTech startup that building the Creator Economy. We are a team of content creators & creative people dedicated to producing high-quality content, innovating and developing digital products, assets, tools, and resources for fellow creators.</p>
+    </div>
   )
 }
