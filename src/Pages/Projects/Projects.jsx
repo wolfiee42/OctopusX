1845d88eae629e6911756c8fa19935a248306a7c
gazinahian <gazinahian.intern@gmail.com>
2024-08-15 09:11:10 +0600

    fixing width alignment and font

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,8 +1,19 @@
 export default function Projects() {
   return (
-    <div className="font-bold text-white bg-slate-950 py-40 px-5 ">
-      <h1 className="text-3xl mb-10 line-clamp-4">About Us</h1>
-      <p className="text-md lg:w-[50%] xl:w-[50%]">OctopusX is a MediaTech startup that building the Creator Economy. We are a team of content creators & creative people dedicated to producing high-quality content, innovating and developing digital products, assets, tools, and resources for fellow creators.</p>
+    <div
+      className="flex justify-center items-center text-white bg-black">
+      <div
+        style={{ fontFamily: 'Inter, sans-serif' }}
+        className="font-bold pt-20 pb-40 px-5 max-w-[75rem]"
+      >
+        <h1 className="text-3xl mb-10 line-clamp-4">About Us</h1>
+        <p className="text-lg lg:w-[60%] xl:w-[60%]">
+          OctopusX is a MediaTech startup that building the Creator Economy. We are
+          a team of content creators & creative people dedicated to producing
+          high-quality content, innovating and developing digital products, assets,
+          tools, and resources for fellow creators.
+        </p>
+      </div>
     </div>
   )
 }
