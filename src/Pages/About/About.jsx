6921e55244e0202a696c3abd7a7a69a0387c1cd8
gazinahian <gazinahian.intern@gmail.com>
2024-08-15 10:49:49 +0600

    fixing padding

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -47,7 +47,7 @@ export default function About() {
       <div>
         <div className="lg:flex justify-center items-center ">
           <div>
-            <h1 className="text-4xl font-bold text-right md:text-5xl lg:text-5xl xl:text-5xl">What We Do</h1>
+            <h1 className="text-4xl font-bold text-right md:text-5xl lg:text-5xl xl:text-5xl pb-9">What We Do</h1>
             <p className="text-lg text-slate-900  xl:text-justify lg:text-justify">This site, OctopusX, is a collection of our ideas and projects that inspire us. We are a team of content creators and creative minds dedicated to producing high-quality content, that what make us independent and bootstrapped. Creating content and working with creators allows us to develop digital products, tools, and resources for fellow creators. We're proud of creating <span className="font-bold underline">ytshops</span> and our commitment to supporting creators and the global creator economy. Explore all of our products <span className="underline">here!</span>
             </p>
           </div>
